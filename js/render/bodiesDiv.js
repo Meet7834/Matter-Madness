9ce09d108aa6d065be204d0f55c192f5bd818509
Meet <proobmeet7834@gmail.com>
2023-10-02 16:42:50 +0530

    added delete functionality

--- js/render/bodiesDiv.js
+++ js/render/bodiesDiv.js
@@ -1,35 +1,52 @@
 const bodiesDiv = document.querySelector("#bodiesDiv");
 
-const createButton = (text, id) => {
-    const btn = document.createElement("button");
-    btn.innerText = text;
-    btn.classList.add(id);
-    return btn;
+const addBodyEventListners = (bodyDiv, infoDiv, btn) => {
+
+    // to hide/unhide the infoDiv 
+    bodyDiv.addEventListener("click", (event) => {
+        if (event.target !== btn) {
+            infoDiv.classList.toggle("hide");
+        }
+    })
+}
+
+const createDeleteButton = (bodyToRemove, divToRemove) => {
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+
+    // adding event listner to the button
+    deleteBtn.addEventListener("click", () => {
+        // remove the body from the world
+        World.remove(engine.world, bodyToRemove);
+
+        // remove the body from our array bodies
+        const index = bodies.indexOf(bodyToRemove);
+        if (index != -1) bodies.splice(index, 1);
+        
+        // Remove the div from the DOM
+        divToRemove.remove();
+    })
+
+    return deleteBtn;
 }
 
 const createInfoDiv = (currBody) => {
     const infoDiv = document.createElement("div");
 
-    infoDiv.innerHTML += `<p>ID: ${currBody.id}</p>`
-    if (currBody.type == "body"){
+    infoDiv.innerHTML += `<p>ID: ${currBody.id}</p>`;
+    if (currBody.type == "body") {
         infoDiv.innerHTML += `<p>Static: ${currBody.isStatic}</p>`
         if (currBody.isStatic) infoDiv.innerHTML += `<p>angle: ${(currBody.angle * 180 / 3.14).toFixed(1)}</p>`
-        
+
         infoDiv.innerHTML += `<p>Friction: ${currBody.friction}</p>`
         infoDiv.innerHTML += `<p>Air Friction: ${currBody.frictionAir}</p>`
         infoDiv.innerHTML += `<p>Static Friction: ${currBody.frictionStatic}</p>`
     }
-    
+
+    infoDiv.classList.add("infoDiv");
     infoDiv.classList.add("hide");
-    
+
     return infoDiv;
-}
-
-const createBodyDiv = (currBody) => {
-    const div = document.createElement("div");
-    div.innerHTML = `<h4>${currBody.label}`;
-
-    return div;
 }
 
 const assembleDiv = (bodyDiv, infoDiv, btn) => {
@@ -38,27 +55,26 @@ const assembleDiv = (bodyDiv, infoDiv, btn) => {
     bodiesDiv.append(bodyDiv);
 }
 
+const createBodyDiv = (currBody) => {
+
+    const bodyDiv = document.createElement("div");
+    bodyDiv.innerHTML = `<h4>${currBody.label}`;
+    bodyDiv.classList.add("bodyDiv");
+
+    const infoDiv = createInfoDiv(currBody);
+    const btn = createDeleteButton(currBody, bodyDiv);
+
+    // event listners:
+    addBodyEventListners(bodyDiv, infoDiv, btn);
+
+    assembleDiv(bodyDiv, infoDiv, btn);
+}
+
+
 const renderBodiesDiv = () => {
     bodiesDiv.innerHTML = "";
     for (let i = 0; i < bodies.length; i++) {
         const currBody = bodies[i];
-        const bodyDiv = createBodyDiv(currBody);
-        const infoDiv = createInfoDiv(currBody);
-        const btn = createButton("Delete", currBody.id);
-
-        assembleDiv(bodyDiv, infoDiv, btn);
-
-        bodyDiv.classList.add("bodyDiv");
-    }
-}
-
-const unhideInfoPage = () => {
-    const allBodyDivs = document.querySelectorAll(".bodyDiv");
-
-    for (let i = 0; i < allBodyDivs.length; i++) {
-        const bodyDiv = allBodyDivs[i];
-        bodyDiv.addEventListener("click", () => {
-            bodyDiv.children[1].classList.toggle("hide"); // make sure to replace 1 with whatever the infoDiv number is at the time.
-        })
+        createBodyDiv(currBody);
     }
 }
--- js/utils/utils.js
+++ js/utils/utils.js
@@ -87,5 +87,4 @@ const resizeCanvas = () => {
 
 const refreshedPage = () => {
     renderBodiesDiv();
-    unhideInfoPage();
 }
